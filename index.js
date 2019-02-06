d7bb379a04b57022e5c27046b8b5b780c6ee3d86
RajeshManikandan <44936837+RajeshManikandan@users.noreply.github.com>
2019-02-06 18:08:24 +0530

    save

--- index.js
+++ index.js
@@ -103,8 +103,7 @@ restService.post('/audio', function(req, res) {
             break;
     }
     return res.json({
-        speech: speech,
-        displayText: speech,
+        fulfillmentText: speech,
         source: 'webhook-echo-sample'
     });
 });
